8f097841df7080cbb4f9519daf6e33015ad758e3
sergiorock <lopez.sergio.n@gmail.com>
2020-08-04 20:22:03 -0300

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :cells
   resources :games do
     post :reveal, on: :member
     post :flag, on: :member
   end
+
+  root to: "games#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
